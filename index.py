bb8085a2580874291d0262b7c3119172f64e86ef
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-01-26 21:38:51 +0200

    Make a simple Calculator v1.0

--- index.py
+++ index.py
@@ -1,1 +1,25 @@
-print("Hello World!")
+# simple calculator v1.0
+
+# 1- input first number 
+firstNumber = int(input("Enter the first number: "))
+
+# 2- input operation type
+operationType = input('Enter the operator do you want: ')
+
+# 3- input second number 
+secondNumber = int(input("Enter the second number: "))
+
+# 4- print the result
+if operationType == "+": 
+     print ("The result =",firstNumber + secondNumber)
+elif operationType == "-":
+     print ("The result =", firstNumber - secondNumber)
+elif operationType == "*":
+     print ("The result =", firstNumber * secondNumber)
+elif operationType == "/":
+     print ("The result =", firstNumber / secondNumber)
+else:
+     print("the operator you entered not valid")
+
+     
+     
